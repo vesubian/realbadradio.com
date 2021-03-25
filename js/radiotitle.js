a3113495683173a2a518b0b1a36d8bb6a8e3ac75
vesubian <georgesimbert@hotmail.com>
2021-03-25 15:16:51 +0100

    modified:   js/radiotitle.js

--- js/radiotitle.js
+++ js/radiotitle.js
@@ -4,7 +4,7 @@ function radioTitle() {
     // this is the URL of the json.xml file located on your server.
     var url = 'http://realbadradio.ddns.net:21000/json.xsl';
     // this is your mountpoint's name, mine is called /radio
-    var mountpoint = '/radio';
+    var mountpoint = '/mpd.mp3';
     $.ajax({  type: 'GET',
           url: url,
           async: true,
