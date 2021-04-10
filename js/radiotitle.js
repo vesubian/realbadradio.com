cb52d9cfdfb936a79379c87349931cc038ecfc81
vesubian <georgesimbert@hotmail.com>
2021-04-10 13:07:38 +0200

    modified:   js/radiotitle.js

--- js/radiotitle.js
+++ js/radiotitle.js
@@ -5,29 +5,25 @@ function radioTitle() {
   var url = 'https://subshapes.com/json.xsl';
   // this is your mountpoint's name, mine is called /radio
   var mountpoint = '/mpd.mp3';
-  $.ajax({  type: 'GET',
-        url: url,
-        async: true,
-        jsonpCallback: 'parseMusic',
-        contentType: "application/json",
-        dataType: 'jsonp',
-        success: function (json) {
-          // this is the element we're updating that will hold the track title
-          $('#track-title').text(json[mountpoint].title);
-          // this is the element we're updating that will hold the listeners count
-          // $('#listeners').text(json[mountpoint].listeners);
-          function updateDiscogsLink() {
-            const discsearch = 'https://www.discogs.com/search/?q=';
-            let disclink = document.getElementById("track-title").innerHTML;
-            disclink =  disclink.replace(/[|]|&|Feat\.|feat\.|ft\.|Featuring|featuring/g, '');
-            let  trimID =  disclink.indexOf("(");
-            if (trimID === -1) { trimID = disclink.length };
-            document.getElementById("discogs-link").href = discsearch + disclink.slice(0, trimID);
-          }
-          updateDiscogsLink()
-      },
-        error: function (e) {    console.log(e.message);
+  $.ajax({
+    type: 'GET',
+    url: url,
+    async: true,
+    jsonpCallback: 'parseMusic',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function (json) {
+      // this is the element we're updating that will hold the track title
+      $('#track-title').text(json[mountpoint].title);
+      function updateDiscogsLink() {
+        const discsearch = 'https://www.discogs.com/search/?q=';
+        let disclink = encodeURIComponent(document.getElementById("track-title").innerHTML);
+        document.getElementById("discogs-link").href = discsearch + disclink;
       }
+      updateDiscogsLink()
+    },
+      error: function (e) {    console.log(e.message);
+    }
   });
 }
 
