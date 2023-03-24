44fb0b4e4c817a20a33c9fa805103c50e72748fe
vesubian <78367891+vesubian@users.noreply.github.com>
2023-03-24 09:53:20 +0100

    Update radiotitle.js

--- js/radiotitle.js
+++ js/radiotitle.js
@@ -1,9 +1,7 @@
 // radiotitle.js
 
 function radioTitle() {
-  // this is the URL of the json.xml file located on your server.
   var url = 'https://stream.realbadradio.com/json2.txt';
-  // this is your mountpoint's name, mine is called /radio
   var mountpoint = '/mpd.mp3';
   $.ajax({  type: 'GET',
         url: url,
@@ -12,21 +10,11 @@ function radioTitle() {
         contentType: "application/json",
         dataType: 'jsonp',
         success: function (json) {
-          // this is the element we're updating that will hold the track title
           $('#track-title').text(json.title);
-          // this is the element we're updating that will hold the listeners count
-          // $('#listeners').text(json[mountpoint].listeners);
           function updateDiscogsLink() {
             const discsearch = 'https://www.discogs.com/search/?q=';
-            //let disclink = document.getElementById("track-title").innerHTML.replace('&amp;', '&');
             let disclink = document.getElementById("track-title").textContent;
-            //disclink = document.getElementById("track-title").textContent.replace(/ *\([^)]*\) */g, "");
-            //disclink =  disclink.replace(/[|]|&amp;|Feat\.|feat\.|ft\.|Featuring|featuring/gi, '');
-            //let trimID =  disclink.indexOf("(") - 1;
-            //if (trimID == -2) { trimID = disclink.length };
-            //document.getElementById("discogs-link").href = discsearch + encodeURIComponent(disclink.slice(0, trimID));
             document.getElementById("discogs-link").href = discsearch + encodeURIComponent(disclink.replace(/ *\([^)]*\) */g, ""));
-            //document.getElementById("discogs-link").href = discsearch + encodeURIComponent(disclink);
           }
           updateDiscogsLink()
       },
